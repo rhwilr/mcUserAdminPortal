e3a47975fa70ed340d6d6d35d231bc339d5740c2
rhwilr <ralphhuwiler@gmail.com>
2015-03-19 19:28:05 +0100

    Changed Design
    using Bootswatch Flatly

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
-
-Route::get('home', 'HomeController@index');
+Route::get('/', ['as' => 'home', 'uses' =>'HomeController@index']);
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
