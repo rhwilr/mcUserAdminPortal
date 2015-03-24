464c2076e8cccbccb7f697bf70eba2597f4b19b4
Ralph Huwiler <ralphhuwiler@gmail.com>
2015-03-24 08:43:39 +0100

    Add Database Migration to TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,6 +1,29 @@
 <?php
 
 class TestCase extends Illuminate\Foundation\Testing\TestCase {
+ 
+ 
+    /**
+    * Default preparation for each test
+    *
+    */
+   public function setUp()
+   {
+       parent::setUp(); // Don't forget this!
+    
+       $this->prepareForTests();
+   }
+
+   /**
+    * Migrates the database and set the mailer to 'pretend'.
+    * This will cause the tests to run quickly.
+    *
+    */
+   private function prepareForTests()
+   {
+       Artisan::call('migrate');
+       Mail::pretend(true);
+   }
 
 	/**
 	 * Creates the application.
